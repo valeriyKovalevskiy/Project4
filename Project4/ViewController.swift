b349183b25223db5a11853f3b843c50885cf23e6
Valeriy Kovalevskiy <vkovalevskiy@wakeapp.ru>
2020-03-11 10:54:07 +0300

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Valeriy Kovalevskiy on 3/11/20.
+//  Copyright © 2020 v.kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
